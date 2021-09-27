a559dd32e3861bc2a46378d623ca6bff8a2f253c
user <y.dubovitsky@gmail.com>
2021-09-27 12:15:11 +0300

    added image service

--- src/app/service/image-upload.service.ts
+++ src/app/service/image-upload.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const IMAGE_API = 'http://localhost:8080/api/image/'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageUploadService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  uploadProfileImage(file: File): Observable<any> {
+    const uploadData = new FormData();
+    uploadData.append('file', file);
+
+    return this.http.post(IMAGE_API + 'upload', uploadData);
+  }
+
+  uploadPostImage(file: File, postId: number): Observable<any> {
+    const uploadData = new FormData();
+    uploadData.append('file', file);
+
+    return this.http.post(IMAGE_API + postId + '/upload', uploadData);
+
+  }
+
+  getProfileImage(): Observable<any> {
+    return this.http.get(IMAGE_API + 'profileImage');
+  }
+
+  getImagePost(postId: number) {
+    return this.http.get(IMAGE_API + postId + '/image');    
+  }
 }
