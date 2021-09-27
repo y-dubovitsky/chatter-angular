351fd3b3fe28462fd45c94892c9cffaacea6b437
user <y.dubovitsky@gmail.com>
2021-09-27 11:02:48 +0300

    added token-storage service

---
+++ src/app/material-module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+
+@NgModule({
+  exports: [
+    MatIconModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatCardModule,
+    MatDialogModule
+  ]
+})
+
+export class MaterialModule {
+
+}
