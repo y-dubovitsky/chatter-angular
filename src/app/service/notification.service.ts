d8859b6773e8ec272b9e78b2f17944fff82bc0e1
user <y.dubovitsky@gmail.com>
2021-09-27 12:07:18 +0300

    added notification service

---
+++ src/app/service/notification.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  constructor(private snackbar: MatSnackBar) { }
+
+  public showNotification(message: string) {
+    this.snackbar.open(message, null, {
+      duration: 2000
+    });
+  }
+}
