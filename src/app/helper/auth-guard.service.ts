bb0133ebe7d73f32db41a8ed35e99c3ef614b7a1
user <y.dubovitsky@gmail.com>
2021-09-27 12:46:12 +0300

    added AuthGuardService

--- src/app/helper/auth-guard.service.ts
+++ src/app/helper/auth-guard.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TokenStorageService } from '../service/token-storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private tokenStorageService: TokenStorageService
+  ) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    const currentUser = this.tokenStorageService.getUser();
+    if(currentUser) {
+      return true;
+    }
+
+    this.router.navigate(['/login'], {
+      queryParams: {
+        returnUrl: state.url
+      }
+    });
+    return false;
+  }
 }
