f420b037b2ffde3918221addb66d3e13b7f95b8d
user <y.dubovitsky@gmail.com>
2021-09-27 12:21:28 +0300

    added comment service

--- src/app/service/comment.service.ts
+++ src/app/service/comment.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const COMMENT_API = 'http://localhost:8080/api/comment/'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addCommentToPost(postId: number, message: string): Observable<any> {
+    return this.http.post(COMMENT_API + postId + '/create', {
+      message: message
+    });
+  }
+
+  getAllCommentsToPost(postId: number): Observable<any> {
+    return this.http.get(COMMENT_API + postId + '/all');
+  }
+
+  deleteComment(commentId: number): Observable<any> {
+    return this.http.post(COMMENT_API + commentId + '/delete', null);
+  }
+
 }
